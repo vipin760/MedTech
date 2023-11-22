dd759042efc79e7099abf93e08c0f04c649a3ade
vipin m <vipinm500@gmail.com>
2023-11-22 15:51:22 +0530

    admin route and component setup here

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path:'',component:AppComponent, loadChildren:()=> import('../app/modules/patient/patient.module').then(com=> com.PatientModule)}
+  {path:'',component:AppComponent, loadChildren:()=> import('../app/modules/patient/patient.module').then(com=> com.PatientModule)},
+  {path:'admin',component:AppComponent, loadChildren:() => import('../app/modules/admin/admin.module').then(com=> com.AdminModule)},
 ];
 
 @NgModule({
--- frontend/src/app/modules/admin/admin-routing.module.ts
+++ frontend/src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component:AdminComponent, children:[
+  {path:'home', component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'', redirectTo:'/admin/login', pathMatch:'full'}
+]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
